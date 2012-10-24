20eaec8ac483c38406d43c204c2f24e7d8b136ae
Jeffrey Lembeck <jlembeck@gmail.com>
2012-10-24 14:10:32 -0700

    Adding Travis to Revolver so it will auto build and test in the future

--- grunt.js
+++ grunt.js
@@ -1,72 +1,74 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: '<json:package.json>',
-    meta: {
-      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
-    },
-    concat: {
-      dist: {
-        src: [
-          '<banner:meta.banner>',
-          '<file_strip_banner:src/revolver.core.js>',
-          '<file_strip_banner:src/carousel.touchdrag.js>',
-          '<file_strip_banner:src/revolver.init.js>'
-        ],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    min: {
-      dist: {
-        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
-        dest: 'dist/<%= pkg.name %>.min.js'
-      }
-    },
-    cssmin: {
-      dist: {
-        src: ['<banner:meta.banner>', 'src/**/*.css'],
-        dest: 'dist/<%= pkg.name %>.min.css'
-      }
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
-    lint: {
-      files: ['grunt.js', 'src/**/*.js', 'test/**/*.js']
-    },
-    watch: {
-      files: '<config:lint.files>',
-      tasks: 'lint qunit'
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        browser: true
-      },
-      globals: {
-        jQuery: true
-      }
-    },
-    uglify: {}
-  });
+	// Project configuration.
+	grunt.initConfig({
+		pkg: '<json:package.json>',
+		meta: {
+			banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+				'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+				'<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+				'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+				' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+		},
+		concat: {
+			dist: {
+				src: [
+					'<banner:meta.banner>',
+					'<file_strip_banner:src/revolver.core.js>',
+					'<file_strip_banner:src/carousel.touchdrag.js>',
+					'<file_strip_banner:src/revolver.init.js>'
+				],
+				dest: 'dist/<%= pkg.name %>.js'
+			}
+		},
+		min: {
+			dist: {
+				src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+				dest: 'dist/<%= pkg.name %>.min.js'
+			}
+		},
+		cssmin: {
+			dist: {
+				src: ['<banner:meta.banner>', 'src/**/*.css'],
+				dest: 'dist/<%= pkg.name %>.min.css'
+			}
+		},
+		qunit: {
+			files: ['test/**/*.html']
+		},
+		lint: {
+			files: ['grunt.js', 'src/**/*.js', 'test/**/*.js']
+		},
+		watch: {
+			files: '<config:lint.files>',
+			tasks: 'lint qunit'
+		},
+		jshint: {
+			options: {
+				curly: true,
+				eqeqeq: true,
+				immed: true,
+				latedef: true,
+				newcap: true,
+				noarg: true,
+				sub: true,
+				undef: true,
+				boss: true,
+				eqnull: true,
+				browser: true
+			},
+			globals: {
+				jQuery: true
+			}
+		},
+		uglify: {}
+	});
 
-  grunt.loadNpmTasks('grunt-css');
-  // Default task.
-  grunt.registerTask('default', 'lint qunit concat min cssmin');
+	grunt.loadNpmTasks('grunt-css');
+	// Default task.
+	grunt.registerTask('default', 'lint qunit concat min cssmin');
+	// Travis
+	grunt.registerTask('travis', 'lint qunit');
 
 };
